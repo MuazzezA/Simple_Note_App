44812a702eeaa57fbd79b0e0c1b005bf3b8036f4
Muazzez Aydın <64336826+MuazzezA@users.noreply.github.com>
2022-01-27 01:24:04 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Note_App',
       theme: softTheme(),
       initialRoute: '/',
